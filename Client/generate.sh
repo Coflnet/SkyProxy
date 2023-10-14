c141d98c10de91bd56319ee977d00f4ec971734d
Äkwav <ekwav@coflnet.com>
2023-10-14 18:53:21 +0200

    update packages

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,8 +1,8 @@
-VERSION=0.1.0
+VERSION=0.1.1
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5029/swagger/v1/swagger.json \
--g csharp-netcore \
+-g csharp \
 -o /local/out --additional-properties=packageName=Coflnet.Sky.Proxy.Client,packageVersion=$VERSION,licenseId=MIT
 
 cd out
