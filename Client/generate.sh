adb9255d23c22ac204e63c96577a179a434798a1
Äkwav <ekwav@coflnet.com>
2024-05-01 22:23:42 +0200

    update client to .net 6

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,14 +1,18 @@
-VERSION=0.1.1
+VERSION=0.1.2
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5029/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Sky.Proxy.Client,packageVersion=$VERSION,licenseId=MIT
+-o /local/out --additional-properties=packageName=Coflnet.Sky.Proxy.Client,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
 
 cd out
-sed -i 's/GIT_USER_ID/Coflnet/g' src/Coflnet.Sky.Proxy.Client/Coflnet.Sky.Proxy.Client.csproj
-sed -i 's/GIT_REPO_ID/SkyProxy/g' src/Coflnet.Sky.Proxy.Client/Coflnet.Sky.Proxy.Client.csproj
-sed -i 's/>OpenAPI/>Coflnet/g' src/Coflnet.Sky.Proxy.Client/Coflnet.Sky.Proxy.Client.csproj
+$path=src/Coflnet.Sky.Proxy.Client/Coflnet.Sky.Proxy.Client.csproj
+sed -i 's/GIT_USER_ID/Coflnet/g' $path
+sed -i 's/GIT_REPO_ID/SkyProxy/g' $path
+sed -i 's/>OpenAPI/>Coflnet/g' $path
+
+sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>README.md</PackageReadmeFile>@g' $path
+sed -i 's@Remove="System.Web" />@Remove="System.Web" />\n    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>@g' $path
 
 dotnet pack
-cp src/Coflnet.Sky.Proxy.Client/bin/Debug/Coflnet.Sky.Proxy.Client.*.nupkg ..
+cp src/Coflnet.Sky.Proxy.Client/bin/Release/Coflnet.Sky.Proxy.Client.*.nupkg ..
