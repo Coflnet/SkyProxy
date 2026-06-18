5cb22a56559daff228f88d8a8ca84a57b2c0be74
Äkwav <ekwav@coflnet.com>
2026-06-19 00:10:33 +0200

    use newtonsoft

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace Coflnet.Sky.Proxy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkyProxy", Version = "v1" });
